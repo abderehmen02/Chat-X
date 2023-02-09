1654664194d18e3c2182e3faae6607c1b66388b6
abderehmen02 <ilahalaha0987@gmail.com>
2023-02-09 17:42:25 +0100

    including nav in home page

--- src/components/nav/unlogedNav.js
+++ src/components/nav/unlogedNav.js
@@ -1,11 +1,8 @@
-import { Typography , Stack , List , ListSubheader ,ListItemButton , ListItemIcon  , ListItemText } from '@mui/material'
+import { Typography , Stack , List , ListSubheader ,ListItemButton , ListItemIcon  , ListItemText, ListItem } from '@mui/material'
 import React from 'react'
-import logo from '../Images/logo.png'
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
-import HomeIcon from '@mui/icons-material/Home';
-import FeedIcon from '@mui/icons-material/Feed';
-
+import logo from '../../Images/logo.png'
+import {motion} from 'framer-motion' ;
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 export const TopNav = ()=>{
     return <Stack><img style={{width : '100px' , height : '100px'}} src={logo} ></img> <Typography> Chat WX  an app  build for communication </Typography></Stack>
@@ -23,29 +20,52 @@ export const SideBar = ()=>{
           Nested List Items
         </ListSubheader>
       }
-    >
+    > <ListItem>          <motion.div
+  className='myLogo' 
+  style={{
+      backgroundImage: `url(${logo})`
+  }}
+    animate={{
+      scale: [1, 1.2, 1.2, 1, 1],
+      rotate: [0, 0, 270, 270, 0],
+      borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+    }}
+    transition={{
+        duration: 1 ,
+        delay: 0.5
+    }}> <motion.div  
+    
+        animate={{
+      scale: [1, 1.2, 1.2, 1, 1],
+      rotate: [0, 0, 270, 270, 0],
+      borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+    }}
+    transition={{
+        duration: 1 ,
+        delay: 0.5
+    }}
+
+    className='logoOverlay' ></motion.div> </motion.div></ListItem>
       <ListItemButton>
         <ListItemIcon>
- <HomeIcon/>         
-        </ListItemIcon>
+ <i className="bi bi-house-door-fill"></i>
+       </ListItemIcon>
         <ListItemText primary="Home" />
       </ListItemButton>
             <ListItemButton>
         <ListItemIcon>
-        <AccountCircleIcon/>
-        </ListItemIcon>
+ <i className="bi bi-person"></i>
+       </ListItemIcon>
         <ListItemText primary="Login" />
       </ListItemButton>
       <ListItemButton>
         <ListItemIcon>
-        <PersonAddIcon/>
-        </ListItemIcon>
+<i className="bi bi-person-plus-fill"></i>        </ListItemIcon>
         <ListItemText primary="Regester" />
       </ListItemButton>
            <ListItemButton>
         <ListItemIcon>
-        <FeedIcon/>
-        </ListItemIcon>
+ <i className="bi bi-newspaper"></i>       </ListItemIcon>
         <ListItemText primary="About" />
       </ListItemButton> 
     </List>
--- src/components/welcome.js
+++ src/components/welcome.js
@@ -6,8 +6,20 @@ import logo from '../Images/logo.png'
 import '../stylesheets/welcome.css'
 import {useHistory} from 'react-router-dom'
 import { auth } from '../firebase' 
+
+
+
+
+
+
+
+import {TopNav  , SideBar} from './nav/unlogedNav'
+
+
+
+
+
 function welcome() {
-  console.log(process.env);
 const history = useHistory()
 
 const signInAsAGuest = ()=>{
@@ -17,79 +29,57 @@ history.push('/landing')
 } 
     return (
         <motion.div initial={{y: '100vh'}} animate={{y: 0}} transition={{duration: 0.5}} className='welcome' >
-       <section className='welcomeBackground' >
-    <h2>
-       Hello October!
-      </h2>
- 
-     <div className="leaf mobileHide" >
-      <div>  <img src="http://www.pngmart.com/files/1/Fall-Autumn-Leaves-Transparent-PNG.png" height="75px" width="75px"/></div>
-      <div><img src="http://www.pngmart.com/files/1/Autumn-Fall-Leaves-Pictures-Collage-PNG.png" height="75px" width="75px"/></div>
-      <div>  <img src="http://www.pngmart.com/files/1/Autumn-Fall-Leaves-Clip-Art-PNG.png" height="75px" width="75px" /></div>
-      <div><img  src="http://www.pngmart.com/files/1/Green-Leaves-PNG-File.png" height="75px" width="75px"/></div>
-       <div> <img src="http://www.pngmart.com/files/1/Transparent-Autumn-Leaves-Falling-PNG.png" height="75px" width="75px"/></div>
-     <div>   <img src="http://www.pngmart.com/files/1/Realistic-Autumn-Fall-Leaves-PNG.png" height="75px" width="75px"/></div>
-     <div><img src="http://cdn.clipart-db.ru/rastr/autumn_leaves_025.png" height="75px" width="75px"/> </div>
-            
-     </div> 
-     
-     <div className="leaf leaf1 mobileHide " >
-     <div>  <img src="http://www.pngmart.com/files/1/Fall-Autumn-Leaves-Transparent-PNG.png" height="75px" width="75px"/></div>
-      <div><img src="http://www.pngmart.com/files/1/Autumn-Fall-Leaves-Pictures-Collage-PNG.png" height="75px" width="75px"/></div>
-      <div>  <img src="http://www.pngmart.com/files/1/Autumn-Fall-Leaves-Clip-Art-PNG.png" height="75px" width="75px" /></div>
-      <div><img  src="http://www.pngmart.com/files/1/Green-Leaves-PNG-File.png" height="75px" width="75px"/></div>
-       <div> <img src="http://www.pngmart.com/files/1/Transparent-Autumn-Leaves-Falling-PNG.png" height="75px" width="75px"/></div>
-     <div>   <img src="http://www.pngmart.com/files/1/Realistic-Autumn-Fall-Leaves-PNG.png" height="75px" width="75px"/></div>
-     <div><img src="http://cdn.clipart-db.ru/rastr/autumn_leaves_025.png" height="75px" width="75px"/></div>
-            
-     </div>
-     
-     <div className="leaf leaf2 mobileHide">
-     <div>  <img src="http://www.pngmart.com/files/1/Fall-Autumn-Leaves-Transparent-PNG.png" height="75px" width="75px"/></div>
-      <div><img src="http://www.pngmart.com/files/1/Autumn-Fall-Leaves-Pictures-Collage-PNG.png" height="75px" width="75px"/></div>
-      <div>  <img src="http://www.pngmart.com/files/1/Autumn-Fall-Leaves-Clip-Art-PNG.png" height="75px" width="75px" /></div>
-      <div><img  src="http://www.pngmart.com/files/1/Green-Leaves-PNG-File.png" height="75px" width="75px"/></div>
 
-       <div> <img src="http://www.pngmart.com/files/1/Transparent-Autumn-Leaves-Falling-PNG.png" height="75px" width="75px"/></div>
-     <div>   <img src="http://www.pngmart.com/files/1/Realistic-Autumn-Fall-Leaves-PNG.png" height="75px" width="75px"/></div>
-     <div><img src="http://cdn.clipart-db.ru/rastr/autumn_leaves_025.png" height="75px" width="75px"/></div>
-            
-     </div>
-     </section>
-
-          <motion.div
-  className='myLogo' 
-  style={{
-      backgroundImage: `url(${logo})`
-  }}
-    animate={{
-      scale: [1, 1.2, 1.2, 1, 1],
-      rotate: [0, 0, 270, 270, 0],
-      borderRadius: ["20%", "20%", "50%", "50%", "20%"],
-    }}
-    transition={{
-        duration: 1 ,
-        delay: 0.5
-    }}> <motion.div  
-    
-        animate={{
-      scale: [1, 1.2, 1.2, 1, 1],
-      rotate: [0, 0, 270, 270, 0],
-      borderRadius: ["20%", "20%", "50%", "50%", "20%"],
-    }}
-    transition={{
-        duration: 1 ,
-        delay: 0.5
-    }}
-
-    className='logoOverlay' ></motion.div> </motion.div> 
           <div className='welcomeTittle'>  welcome to CHAT WX  </div>
           <div className='buttons'  >
      <div className='btn' >       <Button size='large'   variant='contained' color='secondry' ><Link style={{textDecoration : 'none'}} to='/SignIn'>Sign In</Link></Button></div>
       <div className='btn' >      <Button size='large' variant='contained' color='primary' ><Link  style={{textDecoration: 'none'}} to='/SignUp'>Sign Up</Link></Button></div>
       <div className='btn' onClick={signInAsAGuest} > <Button size='large'  variant="contained" color="secondary" > Sign In As A Guest  </Button> </div>
         </div></motion.div>
+
     )
+
+
+
+    //        <section className='welcomeBackground' >
+    // <h2>
+    //    Hello October!
+    //   </h2>
+ 
+    //  <div className="leaf mobileHide" >
+    //   <div>  <img src="http://www.pngmart.com/files/1/Fall-Autumn-Leaves-Transparent-PNG.png" height="75px" width="75px"/></div>
+    //   <div><img src="http://www.pngmart.com/files/1/Autumn-Fall-Leaves-Pictures-Collage-PNG.png" height="75px" width="75px"/></div>
+    //   <div>  <img src="http://www.pngmart.com/files/1/Autumn-Fall-Leaves-Clip-Art-PNG.png" height="75px" width="75px" /></div>
+    //   <div><img  src="http://www.pngmart.com/files/1/Green-Leaves-PNG-File.png" height="75px" width="75px"/></div>
+    //    <div> <img src="http://www.pngmart.com/files/1/Transparent-Autumn-Leaves-Falling-PNG.png" height="75px" width="75px"/></div>
+    //  <div>   <img src="http://www.pngmart.com/files/1/Realistic-Autumn-Fall-Leaves-PNG.png" height="75px" width="75px"/></div>
+    //  <div><img src="http://cdn.clipart-db.ru/rastr/autumn_leaves_025.png" height="75px" width="75px"/> </div>
+            
+    //  </div> 
+     
+    //  <div className="leaf leaf1 mobileHide " >
+    //  <div>  <img src="http://www.pngmart.com/files/1/Fall-Autumn-Leaves-Transparent-PNG.png" height="75px" width="75px"/></div>
+    //   <div><img src="http://www.pngmart.com/files/1/Autumn-Fall-Leaves-Pictures-Collage-PNG.png" height="75px" width="75px"/></div>
+    //   <div>  <img src="http://www.pngmart.com/files/1/Autumn-Fall-Leaves-Clip-Art-PNG.png" height="75px" width="75px" /></div>
+    //   <div><img  src="http://www.pngmart.com/files/1/Green-Leaves-PNG-File.png" height="75px" width="75px"/></div>
+    //    <div> <img src="http://www.pngmart.com/files/1/Transparent-Autumn-Leaves-Falling-PNG.png" height="75px" width="75px"/></div>
+    //  <div>   <img src="http://www.pngmart.com/files/1/Realistic-Autumn-Fall-Leaves-PNG.png" height="75px" width="75px"/></div>
+    //  <div><img src="http://cdn.clipart-db.ru/rastr/autumn_leaves_025.png" height="75px" width="75px"/></div>
+            
+    //  </div>
+     
+    //  <div className="leaf leaf2 mobileHide">
+    //  <div>  <img src="http://www.pngmart.com/files/1/Fall-Autumn-Leaves-Transparent-PNG.png" height="75px" width="75px"/></div>
+    //   <div><img src="http://www.pngmart.com/files/1/Autumn-Fall-Leaves-Pictures-Collage-PNG.png" height="75px" width="75px"/></div>
+    //   <div>  <img src="http://www.pngmart.com/files/1/Autumn-Fall-Leaves-Clip-Art-PNG.png" height="75px" width="75px" /></div>
+    //   <div><img  src="http://www.pngmart.com/files/1/Green-Leaves-PNG-File.png" height="75px" width="75px"/></div>
+
+    //    <div> <img src="http://www.pngmart.com/files/1/Transparent-Autumn-Leaves-Falling-PNG.png" height="75px" width="75px"/></div>
+    //  <div>   <img src="http://www.pngmart.com/files/1/Realistic-Autumn-Fall-Leaves-PNG.png" height="75px" width="75px"/></div>
+    //  <div><img src="http://cdn.clipart-db.ru/rastr/autumn_leaves_025.png" height="75px" width="75px"/></div>
+            
+    //  </div>
+    //  </section>
 }
 
 export default welcome
