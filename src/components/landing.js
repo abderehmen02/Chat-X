ace4ade166fb9ede9cd2875f683d1e386572ed1e
abderehmen02 <ilahalaha0987@gmail.com>
2023-02-12 12:02:28 +0100

    fixing landing page bugs

--- src/components/landing.js
+++ src/components/landing.js
@@ -96,14 +96,14 @@ const VerifyEmail = ()=>{
     </motion.div>
 }
     return (
-        <Stack  sx={{backgroundColor : 'secondary.main' , width : '100vw' , minHeight: '100vh' , alignItems: 'center'  }} > 
+        <Stack  sx={{backgroundColor : 'secondary.dark' , width : '100vw' , minHeight: '100vh' , alignItems: 'center'  }} > 
         <Nav/>
         <Modal open={VerrifyModal} onClose={()=>{setVerrifyModal(false)}}>
            <VerifyEmail/>
       </Modal>
       <Stack borderRadius={1} width="40vw" alignItems="center"  padding={4} spacing={2}  bgcolor="white.light" >
         <TextField fullWidth multiline rows={3}  variant="filled" sx={{width : '100%'  }}  ></TextField>
-        <Stack width='100%' justifyContent="space-between" direction="row" spacing="32px" > <Button variant="outlined"  > Upload File </Button> <Button variant="contained" > Submit </Button> </Stack>
+        <Stack width='100%' justifyContent="space-around" direction="row" spacing="32px" > <Button variant="outlined"  > Upload File </Button> <Button variant="contained" > Submit </Button> </Stack>
       </Stack>
         <Verifymessage className="verify"/>
 <motion.div className='posts' initial={{x: '100vw'}} animate={{x: 0 , transition: {duration: 0.5}}} >
--- src/components/nav/loggedNav.js
+++ src/components/nav/loggedNav.js
@@ -8,7 +8,6 @@ import { useHistory } from 'react-router-dom';
 
 const StyledTextField =  styled(TextField)(({theme})=>({
  '& label': {
-    color: '#fff' ,
     textShadow : 'none'
   },
    '& label.Mui-focused': {
@@ -32,7 +31,11 @@ const StyledTextField =  styled(TextField)(({theme})=>({
 function Nav() {
     const history  = useHistory()
     const userData = useContext(Data)
+    const userdbData = userData.userdbdata
     const userAuthData = userData.user
+   const userdb = userData.userdb
+    console.log(userdbData);
+    console.log(userData);
 
 const showProfile=()=>{
     history.push(`/acount/${userAuthData.uid}`)
@@ -59,12 +62,10 @@ const showProfile=()=>{
         variant="standard"
       />
 
-<Stack direction="row" spacing="8px" alignItems="center" >  <Avatar onClick={showProfile} ></Avatar>      <Typography variant='h4' color="white.light" >Chat WX </Typography>
+<Stack direction="row" spacing="8px" alignItems="center" >  <Avatar onClick={showProfile} src={userdb && userdb.data().ProfilePic} ></Avatar>      <Typography variant='h5' color="white.light" >{userdbData.userName}</Typography>
 </Stack>
 <Button variant="contained" sx={{height : 'inherit'}} >Log out</Button>
     </Stack>
-
-
   )
 }
 
