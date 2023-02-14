1200192cc3c9ce879417068ca2a746a9db100aa1
abderehmen02 <ilahalaha0987@gmail.com>
2023-02-14 15:04:01 +0100

    integrating the nav ui

--- src/components/nav/loggedNav.js
+++ src/components/nav/loggedNav.js
@@ -1,6 +1,7 @@
 import { Stack , styled , Typography , Avatar , List , ListItem , Box, ListItemButton , ListItemText , TextField , Button , InputAdornment  } from '@mui/material'
 import React  , {useContext  , useState , useEffect}from 'react'
 import HomeIcon from '@mui/icons-material/Home';
+import MyLogo from '../../Images/logo.png'
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import { Data } from '../../App';
 import { useHistory , Link} from 'react-router-dom';
@@ -74,13 +75,15 @@ useEffect(() => {
 )
 }, [])
 
+
+
 const MobileNav = ()=>{
   const [ openMobileNav, setOpenModileNav ] = useState(false)
-
-
-
   return <Stack display={{md: 'none'}} >
-<FormatListBulletedIcon  onClick={()=>{setOpenModileNav(false)}} display={!openMobileNav} ></FormatListBulletedIcon>
+<Stack alignItems="center" width='100vw' height="10vh" justifyContent="space-around" direction="row" ><FormatListBulletedIcon  onClick={()=>{setOpenModileNav(false)}} display={!openMobileNav} ></FormatListBulletedIcon> <Stack onClick={showProfile} sx={{cursor: "pointer"}} direction="row" spacing="8px" alignItems="center" >  <Avatar style={{width : '30px' , height  : '30px' }} src={userdb && userdb.data().ProfilePic} ></Avatar>      <Typography sx={{fontSize : '24px'}} color="white.light"  >{  userdbData?.userName}</Typography>
+</Stack>
+<img src={MyLogo} style={{width : '50px' , height : '50px' , borderRadius : '8px'}} ></img>
+</Stack> 
  { openMobileNav && <Stack >
 <StyledTextField
         placeholder='type the whole username'
@@ -109,9 +112,21 @@ const MobileNav = ()=>{
 </Stack> }
   </Stack>
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
   return ( <Box>
 <MobileNav/>
-
   <Stack   display={{xs: 'none' , md : 'flex' }}    alignItems="center"   direction="row" width="100vw"   margin={2} justifyContent="space-around">
 <Stack> 
  <StyledTextField
@@ -135,24 +150,10 @@ const MobileNav = ()=>{
       />
   <Stack sx={{position : 'absolute' , top : '100px' }} spacing="2px" >
 {displayList &&  UserSearched.map(user =>{
-  console.log("user")
- console.log(user.data())
     return           <Typography  bgcolor='standard.main' color='white.main'  onClick={(e)=>{ e.stopPropagation()  ;  history.push(`/acount/${user.id}`)}} sx={{ cursor: 'pointer'  , padding : '8px 32px' , border: '1px solid black'  , borderRadius : '8px'}} >{user.data().FirstName + "   " + user.data().lastName}</Typography>
 })}
 </Stack>
 </Stack>
-
-
-
-
-
-
-
-
-
-
-
-
 <Stack onClick={showProfile} sx={{cursor: "pointer"}} direction="row" spacing="8px" alignItems="center" >  <Avatar  src={userdb && userdb.data().ProfilePic} ></Avatar>      <Typography variant='h5' color="white.light" sx={{"&:hover"  : {color: '#E3E3E3' }}} >{  userdbData?.userName}</Typography>
 </Stack>
 <Button variant="contained" sx={{height : 'inherit'}} onClick={logOut} >Log out</Button>
